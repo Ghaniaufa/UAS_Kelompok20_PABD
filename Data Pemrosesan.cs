2c42eb5b8b1222f38ac2dc833be70659677624d5
Ghaniaufa <maula.ghani.ft21@mail.umy.ac.id>
2023-07-10 20:36:03 +0700

    create new form Data Transaksi and Insert the method refreshform

---
+++ Data Pemrosesan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UAS_Kelompok20_PABD
+{
+    public partial class Data_Pemrosesan : Form
+    {
+        public Data_Pemrosesan()
+        {
+            InitializeComponent();
+        }
+    }
+}
